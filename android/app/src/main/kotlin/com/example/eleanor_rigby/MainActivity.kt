a7ceedcc0591220ecef0a2c3b2c6a27807684b5d
Arie Ridwan <arieridwansyah@gmail.com>
2023-12-25 23:10:14 +0100

    Implement method channel to communicate between flutter and native code

--- android/app/src/main/kotlin/com/example/eleanor_rigby/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/eleanor_rigby/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.example.eleanor_rigby
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), MethodChannel.MethodCallHandler {
+
+    private lateinit var methodChannel:MethodChannel
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "Example")
+        methodChannel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method == "generateLyrics") {
+            val input = call.argument<String>("input")
+            result.success("output: $input")
+        }
+    }
 }
--- lib/features/sketch/data/lyrics_repository_impl.dart
+++ lib/features/sketch/data/lyrics_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'package:eleanor_rigby/features/sketch/data/lyrics_repository.dart';
+import 'package:flutter/services.dart';
 
 class LyricsRepositoryImpl extends LyricsRepository {
+  static const MethodChannel _channel = MethodChannel('Example');
+
   @override
   Future<dynamic> generateLyrics(String input) async {
-    return "Output: $input";
+    return await _channel.invokeMethod('generateLyrics', {"input": input});
   }
 }
