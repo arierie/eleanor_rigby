21e53d993a8825d6c8133847b7b560a1b1b55206
Arie Ridwan <arieridwansyah@gmail.com>
2023-12-27 11:12:47 +0100

    Implement autocomplete from pre-trained model

--- android/app/src/main/kotlin/com/example/eleanor_rigby/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/eleanor_rigby/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.eleanor_rigby
 
+import com.mediamonks.wordfilter.LanguageCheckerImpl
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity: FlutterActivity(), MethodChannel.MethodCallHandler {
 
     private lateinit var methodChannel:MethodChannel
+    private val autoCompleteService: AutoCompleteService by lazy {
+        AutoCompleteServiceImpl(applicationContext, LanguageCheckerImpl())
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -17,8 +24,23 @@ class MainActivity: FlutterActivity(), MethodChannel.MethodCallHandler {
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         if (call.method == "generateLyrics") {
-            val input = call.argument<String>("input")
-            result.success("output: $input")
+            val input = call.argument<String>("input") ?: ""
+            generateLyrics(input, result)
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun generateLyrics(input: String, methodResult: MethodChannel.Result) {
+        GlobalScope.launch {
+            when (val result = autoCompleteService.getSuggestion(input = input)) {
+                is AutoCompleteService.AutoCompleteResult.Success -> {
+                    methodResult.success(result.words)
+                }
+
+                is AutoCompleteService.AutoCompleteResult.Error -> {
+                    methodResult.error("error", "error", "error")
+                }
+            }
         }
     }
 }
