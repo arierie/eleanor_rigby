429c2ea111371d235a325444dd85474d31d7cefa
Arie <arieridwansyah@gmail.com>
2024-02-15 00:32:34 +0100

    Fix model name

--- client/eleanor_rigby.py
+++ client/eleanor_rigby.py
@@ -4,9 +4,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 hf_token = st.secrets["hf_token"]
-subprocess.run(["huggingface-cli", "login", "--token", hf_token])
-
-inference_model = AutoModelForCausalLM.from_pretrained("arieridwans/phi-2-finetuned-lyrics", trust_remote_code=True, torch_dtype=torch.float32)
+inference_model = AutoModelForCausalLM.from_pretrained("arieridwans/phi_2-finetuned-lyrics", trust_remote_code=True, torch_dtype=torch.float32, token=hf_token)
 inference_tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", use_fast=True)
 inference_tokenizer.pad_token=inference_tokenizer.eos_token
 
