db8d2902d0d92761d29633c28cb0f4cae01fe235
Arie <arieridwansyah@gmail.com>
2024-02-15 20:43:02 +0100

    Update eleanor_rigby.py

--- client/eleanor_rigby.py
+++ client/eleanor_rigby.py
@@ -9,8 +9,6 @@ output = ""
 
 hf_token = st.secrets["hf_token"]
 inference_model = AutoModelForCausalLM.from_pretrained("arieridwans/phi_2-finetuned-lyrics", trust_remote_code=True, torch_dtype=torch.float32, token=hf_token)
-inference_tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", use_fast=True)
-inference_tokenizer.pad_token=inference_tokenizer.eos_token
 
 if st.button("Generate Output"):
   st.text("Generated Output:")
