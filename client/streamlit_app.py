6fd0ce43d14a4bda9ef146a7688b87aab3efdc07
Arie <arieridwansyah@gmail.com>
2024-02-22 08:57:08 +0100

    Update streamlit_app.py

--- client/streamlit_app.py
+++ client/streamlit_app.py
@@ -3,32 +3,15 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="Eleanor Rigby")
+st.title('Eleanor Rigby: The Beatles-inspired lyrics generation')
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
-
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    st.subheader('Parameters')
+    llm = 'https://replicate.com/arierie/phi_2-finetuned-lyrics:48a60dd8d863735436eed0a6ff3b108d84fd333321c75441c57ac6a7cd75dbeb'
+    max_token = st.sidebar.slider('max_length', min_value=128, max_value=1028, value=512, step=8)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -51,9 +34,9 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    output = replicate.run('arierie/phi_2-finetuned-lyrics:48a60dd8d863735436eed0a6ff3b108d84fd333321c75441c57ac6a7cd75dbeb', 
+                           input={"user_prompt": "{prompt_input}",
+                                  "max_new_tokens": max_token})
     return output
 
 # User-provided prompt
